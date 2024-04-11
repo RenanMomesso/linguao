5fad4e87dca41d4a32ee364ecd826d2d073f1671
Renan Momesso <renan_momesso@hotmail.com>
2024-04-11 20:50:33 -0300

    change eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: '@react-native-community',
+  rules: {
+    quotes: ['error', 'double', {allowTemplateLiterals: true}],
+    'linebreak-style': 0,
+    'react/react-in-jsx-scope': 'off',
+  },
 };
