392d7189643c00b85a7ef22ec355d241af4f6ad3
Renan Momesso <renan_momesso@hotmail.com>
2024-04-11 17:36:04 -0300

    add svg and svg transformer to the list of dependencies     dependencies: {         'svg': 'svg',         'svg.transform': 'svg.transform'     } });

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,30 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const {
+  getDefaultConfig: getRNDefaultConfig,
+  mergeConfig,
+} = require('@react-native/metro-config');
+const {getDefaultConfig: getExpoDefaultConfig} = require('expo/metro-config');
 
-/**
- * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {};
+module.exports = (() => {
+  // Get the default React Native and Expo configurations
+  const rnConfig = getRNDefaultConfig(__dirname);
+  const expoConfig = getExpoDefaultConfig(__dirname);
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+  // Merge the React Native and Expo configurations
+  let mergedConfig = mergeConfig(rnConfig, expoConfig);
+
+  // Apply additional configurations for react-native-svg-transformer
+  const {transformer, resolver} = mergedConfig;
+
+  mergedConfig.transformer = {
+    ...transformer,
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  };
+
+  mergedConfig.resolver = {
+    ...resolver,
+    assetExts: resolver.assetExts.filter(ext => ext !== 'svg'),
+    sourceExts: [...resolver.sourceExts, 'svg'],
+  };
+
+  return mergedConfig;
+})();
